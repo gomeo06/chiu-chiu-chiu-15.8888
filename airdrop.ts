4eedcadc0f0e96c3c96646581f6ad386d3151eec
Xich Bich <139754806+gomeo06@users.noreply.github.com>
2023-08-15 14:02:13 +0700

    Create airdrop.ts

---
+++ airdrop.ts
@@ -0,0 +1,76 @@
+import { NowRequest, NowResponse } from '@now/node'
+import faunadb from 'faunadb'
+import { Wallet } from '@ethersproject/wallet'
+
+import { AddressDocument } from '../../constants'
+import { validatePermissionString, getFaucetData, faucet, timeoutPromise } from '../../utils'
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+const q = faunadb.query
+
+export default async function(req: NowRequest, res: NowResponse): Promise<NowResponse> {
+  const { body } = req
+  const { address, signature, scannedAddress } = JSON.parse(body || JSON.stringify({}))
+
+  if (!address || !signature || !scannedAddress) {
+    return res.status(400).send('')
+  }
+
+  if (address === scannedAddress) {
+    return res.status(400).send('')
+  }
+
+  if (!validatePermissionString(address, signature)) {
+    return res.status(401).send('')
+  }
+
+  try {
+    const addressData: any = await client.query(
+      q.Map(q.Paginate(q.Match(q.Index('by-address_addresses'), address)), (ref): any => q.Get(ref))
+    )
+
+    if (addressData.data.length === 0) {
+      return res.status(401).send('')
+    }
+
+    const addressDocument: AddressDocument = addressData.data[0].data
+
+    if (addressDocument.boostsLeft === 0) {
+      return res.status(401).send('')
+    }
+
+    // faucet both
+    let faucetError = false
+    try {
+      const faucetWallet = new Wallet(process.env.FAUCET_PRIVATE_KEY)
+      const signature = await faucetWallet.signMessage(getFaucetData(address))
+      const signatureScanned = await faucetWallet.signMessage(getFaucetData(scannedAddress))
+      await Promise.all([
+        timeoutPromise(faucet(address, signature)),
+        timeoutPromise(faucet(scannedAddress, signatureScanned))
+      ])
+    } catch (error) {
+      console.error(error)
+      faucetError = true
+    }
+
+    if (faucetError) {
+      return res.status(500).send('An unknown error occurred.')
+    } else {
+      await client.query(
+        q.Update(q.Ref(q.Collection('addresses'), addressData.data[0].ref.id), {
+          data: {
+            boostsLeft: addressDocument.boostsLeft - 1
+          }
+        })
+      )
+
+      return res.status(200).send('')
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send('An unknown error occurred.')
+  }
+}
